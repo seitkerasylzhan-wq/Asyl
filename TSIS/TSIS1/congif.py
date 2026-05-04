8ee0b387da1402658d5db50b5c05fc4146f77799
seitkerasylzhan-wq <seitkerasylzhan@gmail.com>
2026-05-04 15:33:04 +0500

    Create congif.py

---
+++ TSIS/TSIS1/congif.py
@@ -0,0 +1,16 @@
+def load_config():
+    return {
+        "host": "localhost",
+        "database": "phonebook_db",
+        "user": "postgres",
+        "password": "q3e1t7u5",
+    }
+
+
+def load_admin_config():
+    return {
+        "host": "localhost",
+        "database": "postgres",
+        "user": "postgres",
+        "password": "q3e1t7u5",
+    }
